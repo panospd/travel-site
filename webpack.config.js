dc64d71b9994b802ede2aec76fb2e14e310c194b
panospd <anastapd@gmail.com>
2020-12-03 09:07:01 +0000

    Implemented basic workflow automation for css and js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const postCSSPlugins = [
+  "postcss-simple-vars",
+  "postcss-nested",
+  "autoprefixer",
+];
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
